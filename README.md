5101c8f671f6a571b5fbd0a1e2e7342dce532c78
Siver K. Volle <8384586+siverv@users.noreply.github.com>
2021-07-06 00:45:23 +0200

    DevOps-work: Everything combined into one docker (only dev-server at the moment)

--- README.md
+++ README.md
@@ -14,46 +14,21 @@
 
 Follow the instructions to install [docker](https://docs.docker.com/engine/install/ubuntu/) and [docker-compose](https://docs.docker.com/compose/install/)
 
-#### Python
-
-Ensure you're using python 3.x.
-
-Install python's [virtualenv](https://virtualenv.pypa.io/en/latest/) systemwide
-
-Install [flask](https://flask.palletsprojects.com/en/2.0.x/
-
-```
-cd $REPO/api
-virtualenv .venv;
-.venv/bin/pip install flask pymysql flask-cors
-```
-
-
-### Starting
-
-#### Docker
-
 ```
 cd $REPO
 docker-compose up -d
 ```
 
-#### Python API
+To set up (or reset) the initial database in mysql:
 
 ```
-cd $REPO/api
-source .venv/bin/activate
-export FLASK_APP=server
-flask run
+docker-compose exec mysql mysql -p
+< # Enter the password from the `./secrets/<env>/mysql_root_password
+mysql> source /app/db/initial-tables.sql 
 ```
 
-The api should now be running on `localhost:5000`
-
-#### Python Web
+To see the logs from the api-server:
 
 ```
-cd $REPO/web
-python -m http.server
+docker-compose logs -f api
 ```
-
-The web-files should now be server on `localhost:8000`
--- api/server.py
+++ api/server.py
@@ -2,18 +2,39 @@ from flask import Flask, request, jsonify, g
 from flask_cors import CORS
 import pymysql.cursors
 import json
+import sys
+from dotenv import dotenv_values
 
 app = Flask(__name__)
 CORS(app)
 
+env = dotenv_values("/run/secrets/flask.env")
+if env is None:
+    print("This is run without docker")
+    sys.exit(1)
+
+mysql_host = env["FLASK_MYSQL_HOST"]
+mysql_port = env["FLASK_MYSQL_PORT"]
+
+if mysql_port is None:
+    print("No FLASK_MYSQL_PORT env variable set")
+else:
+    mysql_port = int(mysql_port)
+
+mysql_user = env["FLASK_MYSQL_USER"];
+mysql_user_password = open("/run/secrets/mysql_user_password").readline().rstrip()
+
+mysql_database = env["FLASK_MYSQL_DATABASE"]
+
+
 
 def connect_db():
     return pymysql.connect(
-        host='localhost',
-        port=23306,
-        user='root',
-        password='testtest123',
-        db='sultstua',
+        host=mysql_host,
+        port=mysql_port,
+        user=mysql_user,
+        password=mysql_user_password,
+        db=mysql_database,
         autocommit=True,
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
